724ee7a9cccb3f1378e6b76a0b02af98f4b76d44
Rob Wallator <rob@rawimpressions.ca>
2020-07-27 03:46:07 +0000

    working on dropdown

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,31 +1,27 @@
 $(document).ready(() => {
-  $("#homeButton").click(() => {
-    $.ajax({
-      method: "GET",
-        url: "/"
-    }).done((res)=>{
-      console.log("homebutton");
-      console.log(res);
-    })
-  })
-  $("#mapButton").click(() => {
+  $("#card1").click(() => {
     $.ajax({
       method: "GET",
         url: "/map"
     }).done((res)=>{
-      console.log("mapbutton");
-      console.log(res);
-    })
-    .catch((err)=>{
-      console.log(err);
+      window.location.replace("/map");
     })
   })
+  // $("#mapButton").click(() => {
+  //   $.ajax({
+  //     method: "GET",
+  //       url: "/map"
+  //   }).done((res)=>{
 
+  //   })
+  //   .catch((err)=>{
+  //     console.log(err);
+  //   })
+  // })
   $("#loginButton").on("click", function(event) {
     event.preventDefault();
     $("#dropHeader").slideDown(400);
   })
-  // $("#loginButton").on( "click", function() {
-  //   $(this).console.log("Button was clicked");
-  // });
+
+
 })
