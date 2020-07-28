a15483bfc42288d3960e6831441ad05891395c6c
Rob Wallator <rob@rawimpressions.ca>
2020-07-28 03:00:27 +0000

    CSS/HTML small changes

--- routes/users.js
+++ routes/users.js
@@ -21,8 +21,10 @@ module.exports = (db) => {
           .json({ error: err.message });
       });
   });
-  // router.get("/",(req,res) =>{
-  //   res.render('map');
-  // })
+  router.get("/",(req,res) =>{
+    res.render('map');
+  })
+
+  
   return router;
 };
